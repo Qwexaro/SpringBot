d2881d9e71e7f2f903118200609747f32bacfd89
Qwexaro <128168133+Qwexaro@users.noreply.github.com>
2025-02-01 15:43:19 +0300

    Update BotConfig.java

--- SpringDemoBot/src/main/java/proj3ct/SpringDemoBot/config/BotConfig.java
+++ SpringDemoBot/src/main/java/proj3ct/SpringDemoBot/config/BotConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Data //анотация ксоздающая автоматические конструкторы
+@Data //анотация создающая автоматические конструкторы
 @PropertySource("application.properties")
 public class BotConfig {
     @Value("${bot.name}")
