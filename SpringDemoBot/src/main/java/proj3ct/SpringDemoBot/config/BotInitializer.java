4b3006f5c917cc9fc46da8c17de936cbbec8cd7e
Qwexaro <Sergikstar25.gold@mail.ru>
2025-02-01 15:40:56 +0300

    main

---
+++ SpringDemoBot/src/main/java/proj3ct/SpringDemoBot/config/BotInitializer.java
@@ -0,0 +1,29 @@
+package proj3ct.SpringDemoBot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import proj3ct.SpringDemoBot.service.TelegramBot;
+
+@Component
+public class BotInitializer {
+    @Autowired
+    TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        }
+        catch (TelegramApiException e){
+
+        }
+
+    }
+}
---
+++ SpringDemoBot/src/test/java/proj3ct/SpringDemoBot/SpringDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package proj3ct.SpringDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
